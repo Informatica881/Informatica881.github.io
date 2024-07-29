77e5d1420e77fa079310373beaf86bb5c282da41
Hicortab <Hicortab@users.noreply.github.com>
2024-07-29 12:23:05 +0200

    Alcuni aggiustamenti

--- wp-content/themes/twentytwentyfour/patterns/gallery-full-screen-image.php
+++
@@ -1,6 +0,0 @@
-
-<!-- wp:cover {"url":"<br />
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_url() in /var/www/html/wp-content/themes/twentytwentyfour/patterns/gallery-full-screen-image.php:10
-Stack trace:
-#0 {main}
-  thrown in <b>/var/www/html/wp-content/themes/twentytwentyfour/patterns/gallery-full-screen-image.php</b> on line <b>10</b><br />
--- wp-content/themes/twentytwentyfour/patterns/testimonial-centered.php
+++
@@ -1,6 +0,0 @@
-
-<!-- wp:group {"metadata":{"name":"<br />
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html_x() in /var/www/html/wp-content/themes/twentytwentyfour/patterns/testimonial-centered.php:12
-Stack trace:
-#0 {main}
-  thrown in <b>/var/www/html/wp-content/themes/twentytwentyfour/patterns/testimonial-centered.php</b> on line <b>12</b><br />
